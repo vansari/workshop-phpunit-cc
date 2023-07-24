b6e655fd4c66120bc879ae603c072ccb90dcaf8b
vansari <kurt.junker@posteo.de>
2023-07-24 17:21:30 +0200

    Better directory added to gitignore

--- tests/unit/unmocked_no_cov/AppNoMockNoCoverageTest.php
+++ tests/unit/unmocked_no_cov/AppNoMockNoCoverageTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace vansari\workshop\phpunit\Tests\unit\unmocked_no_cov;
 
-use Mockery;
 use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
 use vansari\workshop\phpunit\App;
